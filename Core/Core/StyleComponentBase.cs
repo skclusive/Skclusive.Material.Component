bb4f99181e9b5693f54577c5dc8d04c6a960bf7c
skclusive <skclusive@outlook.com>
2019-12-19 02:47:58 +0530

    Styles components added

---
+++ Core/Core/StyleComponentBase.cs
@@ -0,0 +1,13 @@
+﻿using Skclusive.Core.Component;
+
+namespace Skclusive.Material.Core
+{
+    public class StyleComponentBase : StaticComponentBase
+    {
+        protected readonly string media = "@media";
+
+        protected readonly string keyframes = "@keyframes";
+
+        protected readonly string webkitKeyframes = "@-webkit-keyframes";
+    }
+}
